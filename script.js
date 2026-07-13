4a38efcca58a6a7730aca04a4fd2c3d678cc4f80
Precious Oyebanjo <preciousoyebanjo@globusbank.com>
2026-07-13 11:18:43 +0100

    Added a delete button and read more post functionality.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function createPost(title, body, author) {
         title: title,
         body: body,
         author: author,
-        date: new Date().toLocaleDateString()
+        date: new Date().toLocaleDateString() 
     };
     return post;
 }
@@ -40,7 +40,11 @@ function renderPosts(keyword) {
         container.innerHTML += "<h2 data-postid=" + p.id + ">" + p.title + "</h2>";
         container.innerHTML += "<p>" + p.date + " - " + p.author + "</p>";
         container.innerHTML += "<p>" + preview + "...</p>";
-        container.innerHTML += "<hr>";
+        container.innerHTML += "<div class='postActions'>";
+        container.innerHTML += "<button data-postid=" + p.id + " class='readMoreBtn'>Read More</button>";
+        container.innerHTML += "<button data-postid=" + p.id + " class='deleteBtn'>Delete</button>";
+        container.innerHTML += "</div>";
+        container.innerHTML += "<hr>"; // a horizontal line to separate posts
     }
 }
 
@@ -54,24 +58,61 @@ function viewPost(id) {
     }
 
     document.getElementById("postTitle").innerHTML = thePost.title;
+    document.getElementById("postTitle").setAttribute("data-postid", thePost.id);
     document.getElementById("postMeta").innerHTML = thePost.date + " - " + thePost.author;
     document.getElementById("postBody").innerHTML = thePost.body;
+
+    var relatedContainer = document.getElementById("relatedPosts");
+    relatedContainer.innerHTML = "";
+    var count = 0;
+
+    for (var i = 0; i < posts.length; i++) {
+        if (posts[i].id != id && count < 3) {
+            relatedContainer.innerHTML += "<p data-postid=" + posts[i].id + " class='relatedPostLink'>" + posts[i].title + "</p>";
+            count++;
+        }
+    }
 
     document.getElementById("postList").style.display = "none";
     document.getElementById("postView").style.display = "block";
     document.getElementById("addPostButton").style.display = "none";
+    document.getElementById("searchInput").style.display = "block";
+}
+
+function deletePost(id) {
+    posts = posts.filter(function (post) {
+        return post.id != id;
+    });
+
+    savePosts();
+    renderPosts();
 }
 
 document.getElementById("searchInput").addEventListener("input", function () {
     renderPosts(this.value);
 });
 
+
 document.getElementById("postList").addEventListener("click", function (event) {
-    if (event.target.tagName === "H2") {
+    if (event.target.tagName === "H2" || event.target.classList.contains("readMoreBtn")) {
         var clickedId = event.target.getAttribute("data-postid");
         viewPost(clickedId);
     }
+
+    if (event.target.classList.contains("deleteBtn")) {
+        var clickedId = event.target.getAttribute("data-postid");
+        if (confirm("Are you sure you want to delete this post?")) {
+            deletePost(clickedId);
+        }
+    }
 });
+
+document.getElementById("relatedPosts").addEventListener("click", function (event) {
+    if (event.target.classList.contains("relatedPostLink")) {
+        var clickedId = event.target.getAttribute("data-postid");
+        viewPost(clickedId);
+    }
+})
 
 document.getElementById("backButton").addEventListener("click", function () {
     document.getElementById("postList").style.display = "block";
@@ -115,6 +156,16 @@ document.getElementById("submitPostButton").addEventListener("click", function (
     document.getElementById("addPostView").style.display = "none";
     document.getElementById("postList").style.display = "block";
     document.getElementById("addPostButton").style.display = "block";
+});
+
+document.getElementById("deleteFromViewButton").addEventListener("click", function () {
+    var currentId = document.getElementById("postTitle").getAttribute("data-postid");
+    if (confirm("Are you sure you want to delete this post?")) {
+        deletePost(currentId);
+        document.getElementById("postView").style.display = "none";
+        document.getElementById("postList").style.display = "block";
+        document.getElementById("addPostButton").style.display = "block";
+    }
 });
 
 loadPosts();
